c28aa349bd0c80b64860b8799c8c2e6b6ce5ce28
probuing <wangxin330261819@163.com>
2015-05-02 19:32:23 +0800

    Initial commit

---
+++ app/src/main/java/com/puddingvideo/Activity/ActivitySearch.java
@@ -0,0 +1,19 @@
+package com.puddingvideo.Activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.puddingvideoproject.R;
+
+public class ActivitySearch extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+
+}
---
+++ app/src/main/java/com/puddingvideo/Activity/ActivityTime.java
@@ -0,0 +1,20 @@
+package com.puddingvideo.Activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.puddingvideoproject.R;
+
+public class ActivityTime extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_time);
+    }
+
+
+
+}
